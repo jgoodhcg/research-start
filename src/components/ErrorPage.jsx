35626a0b7fc480606018b1545c32444c80470dbb
jgoodhcg <jgoodhcg@gmail.com>
2017-05-25 01:35:51 -0400

    add error page component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { Card, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 class ErrorPage extends Component {
-    render(){
-        return(<div>error page</div>)
+    render() {
+        return (
+            <div className="content" style={{ padding: "0.5em" }}>
+                <Card>
+                    <CardText>There was an error accessing the requested subject,
+                        please refer to the list of available subject
+                    </CardText>
+                     <FlatButton
+                        alt="available_subject_list"
+                        label="Subject List"
+                        href="/"
+                        fullWidth
+                        backgroundColor="#0065a4"
+                        labelStyle={{color: "white"}}
+                    />
+
+                </Card>
+            </div>
+        )
     }
 }
 
